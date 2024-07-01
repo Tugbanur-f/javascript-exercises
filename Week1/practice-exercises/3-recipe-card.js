ddfd8b4a7e9eaba9aa2d544c25388b33459f3e8e
Tugbanur <tnur.yurtsever@gmail.com>
2024-07-01 23:52:11 +0200

    h

--- Week1/practice-exercises/3-recipe-card.js
+++ Week1/practice-exercises/3-recipe-card.js
@@ -13,21 +13,21 @@
  */
 
 let myRecipe = {
- title: Omelette;
- servings: 2;
+ title: Omelette,
+ servings: 2,
  ingredients: [
     "4 eggs",
     "2 strips of bacon",
     "1 tsp salt/pepper"
- ];
+ ]
 };
 
     for (let property in myRecipe) {
-      if (property === 'title') {
-        console.log("Meal name:", myRecipe[property]); /
-      } else if(property === 'servings') {
+      if (property == 'title') {
+        console.log("Meal name:", myRecipe[property]); 
+      } else if(property == 'servings') {
         console.log("Serves:", myRecipe[property]); 
-      } else if (property === 'ingredients') {
+      } else if (property =='ingredients') {
         console.log("Ingredients:", myRecipe[property].join(', ')); 
       }
     }
