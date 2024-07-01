dea67440561443d37e5ed218a3a40f0d5106b553
Tugbanur <tnur.yurtsever@gmail.com>
2024-07-01 23:47:42 +0200

    recipe

--- Week1/practice-exercises/3-recipe-card.js
+++ Week1/practice-exercises/3-recipe-card.js
@@ -12,3 +12,22 @@
  *   Ingredients: 4 eggs, 2 strips of bacon, 1 tsp salt/pepper
  */
 
+let myRecipe = {
+ title = Omelette;
+ servings = 2;
+ ingredients = [
+    "4 eggs",
+    "2 strips of bacon",
+    "1 tsp salt/pepper"
+ ];
+};
+
+    for (let property in myRecipe) {
+      if (property === 'title') {
+        console.log("Meal name:", myRecipe[property]); /
+      } else if(property === 'servings') {
+        console.log("Serves:", myRecipe[property]); 
+      } else if (property === 'ingredients') {
+        console.log("Ingredients:", myRecipe[property].join(', ')); 
+      }
+    }
