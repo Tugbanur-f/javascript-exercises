e8429f84461caceda3141114fa9c8b9e34bec551
Tugbanur <tnur.yurtsever@gmail.com>
2024-07-08 14:59:13 +0200

    modified

--- Week2/prep-exercises/2-experiments/index.js
+++ Week2/prep-exercises/2-experiments/index.js
@@ -9,16 +9,7 @@ function runExperiment(sampleSize) {
   valueCount[randomValue - 1]++;
   }
     
-  // TODO
-  // Write a for loop that iterates `sampleSize` times (sampleSize is a number).
-  // In each loop iteration:
-  //
-  // 1. Generate a random integer between 1 and 6 (as if throwing a six-sided die).
-  // 2. Add `1` to the element of the `valueCount` that corresponds to the random
-  //    value from the previous step. Use the first element of `valueCounts`
-  //    for keeping a count how many times the value 1 is thrown, the second
-  //    element for value 2, etc.
-
+  
   const results = [];
 
 for(let count of valueCount){
@@ -26,15 +17,7 @@ for(let count of valueCount){
  results.push(percentage);
 }
   
-  // TODO
-  // Write a for..of loop for the `valueCounts` array created in the previous
-  // loop. In each loop iteration:
-  // 1. For each possible value of the die (1-6), compute the percentage of how
-  //    many times that value was thrown. Remember that the first value of
-  //    `valueCounts` represent the die value of 1, etc.
-  // 2. Convert the computed percentage to a number string with a precision of
-  //    two decimals, e.g. '14.60'.
-  // 3. Then push that string onto the `results` array.
+
 
   return results;
 }
@@ -49,11 +32,7 @@ function main() {
 
   
 
-  // TODO
-  // Write a for..of loop that calls the `runExperiment()` function for each
-  // value of the `sampleSizes` array.
-  // Log the results of each experiment as well as the experiment size to the
-  // console.
+  
   // The expected output could look like this:
   //
   // [ '26.00', '17.00', '10.00', '19.00', '16.00', '12.00' ] 100
